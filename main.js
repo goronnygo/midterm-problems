277f4b897e8a7368fbb5dd8f80ced218132e07aa
Ronny <ronnykuruvilla@gmail.com>
2021-03-08 15:02:33 -0500

    half complete

--- main.js
+++ main.js
@@ -1,31 +1,83 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1,num2) {
+  if (num1 % num2 === 0){
+    return true;
+  }else return false;
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  let value = 0;
+  value = num ** 2 / 1/2;
+    return value;
 }
 
-function isLong() {
+function isLong(str) {
+  if (str.length >= 15){
+    return true;
+  }if (str.length < 15)
+    return false;
 }
 
-function exclaim() {
+function exclaim(str) {
+  if (str.length-1 !== '!')
+  return str + '!';
+  if (str.length-1 === '!')
+  return str;
 }
 
-function countWords() {
+function countWords(str) {
+  let total = 0;
+  for (let i = 0; i < countWords.length; i++)
+    if (str(i) === " ") {
+      total = +1;
+  }return totalSoFar;
 }
 
-function containsDigit() {
+function containsDigit(num) {
+  digit = 0;
+  for (i = 0; i < str.length; i++){
+    if (str[i] === 0
+      || str [i] === 1
+      || str [i] === 2
+      || str [i] === 3
+      || str [i] === 4
+      || str [i] === 5
+      || str [i] === 6
+      || str [i] === 7
+      || str [i] === 8
+      || str [i] === 9){
+      digit += 1;
+    }
+  } if (digit < 1){
+    return false;
+  } else {
+    return true;
+  }
 }
 
-function containsLowerCase() {
+
+function containsLowerCase(str) {
+  if (str.toLowerCase === str){
+    return true;
+  }else {
+    return false;
+  }
 }
 
-function containsUpperCase() {
+function containsUpperCase(str) {
+  if (str.toUpperCase === str){
+    return true;
+  }else {
+    return false;
+  }
 }
 
 function containsNonAlphanumeric() {
 }
 
-function containsSpace() {
+function containsSpace(str) {
+  if (str === ' '|| str === '  ' || str === '   '){
+    return true;
+  }
 }
 
 function digits() {
@@ -34,7 +86,16 @@ function digits() {
 function truncate() {
 }
 
-function isValidPassword() {
+function isValidPassword(str) {
+  if (str.toUpperCase() === str 
+  && str.toLowerCase() === str 
+  && str.isNaN() === str 
+  && str.containsNonAlphanumeric()=== str
+  && str.containsNoSpace() === str){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function onlyPunchy() {
